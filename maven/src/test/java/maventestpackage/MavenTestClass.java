cfc85179f324650ee9f3e2ac4fd93c698d9567d4
arati.datir <datirarati777@gmail.com>
2022-12-13 13:17:59 +0530

    first commit

---
+++ maven/src/test/java/maventestpackage/MavenTestClass.java
@@ -0,0 +1,19 @@
+package maventestpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MavenTestClass
+{
+	
+	@Test
+	public void test() 
+	{
+		WebDriver driver = new ChromeDriver();
+		
+		driver .get("https://google.com");
+		
+	}
+
+}
